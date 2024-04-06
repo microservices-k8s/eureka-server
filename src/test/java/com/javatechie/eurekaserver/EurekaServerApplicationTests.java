57219f0d9b5908f9677fef468c8bd4c3bdc89e69
Pavankalyan Magam <pavankalyanmagam@gmail.com>
2024-04-06 13:10:38 -0500

    initial commit

---
+++ src/test/java/com/javatechie/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
